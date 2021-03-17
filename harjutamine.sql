e925eb7d9fad5439f607e9485e0cf954e6e530ae
Risto Tõldsep <r.toldsep@gmail.com>
2021-03-17 18:23:52 +0200

    17.03 praktikumi sql lisatud

--- harjutamine.sql
+++ harjutamine.sql
@@ -46,7 +46,76 @@ SELECT kanne_id, konto_id, kande_aeg, summa, 0 + konto AS kontojääk
 FROM Kanne 
 ORDER BY konto_id, kande_aeg;
 
+/* 10.03.2021 praktikum */
+SELECT Oppimine.*, Tudeng.eesnimi & ' ' & Tudeng.perenimi AS tudengi_nimi, Oppejoud.perenimi AS oppejou_perenimi, Aine.nimetus AS aine_nimetus
+FROM Oppimine, Tudeng, Oppejoud, Aine
+WHERE oppim_algus >= #1998-01-01# AND oppim_algus <= #1999-01-01# 
+AND Oppimine.tudeng=Tudeng.tudkood
+AND Oppimine.oppejoud = Oppejoud.oppejou_kood
+AND Oppimine.aine=Aine.aine_kood
+AND Oppimine.oppimine IN (SELECT oppimine
+FROM Eksam 
+WHERE tulemus IN (4,5));
+
+-- VANEM SÜNTAKS tabelite ühendamisel
+-- Older syntax for joining tables
+SELECT Oppimine.*, Tudeng.eesnimi & ' ' & Tudeng.perenimi AS tudengi_nimi, Oppejoud.perenimi AS oppejou_perenimi, Aine.nimetus AS aine_nimetus
+FROM Oppimine, Tudeng, Oppejoud, Aine
+WHERE oppim_algus >= #1998-01-01# AND oppim_algus <= #1999-01-01# 
+AND Oppimine.tudeng=Tudeng.tudkood
+AND Oppimine.oppejoud = Oppejoud.oppejou_kood
+AND Oppimine.aine=Aine.aine_kood
+AND EXISTS (SELECT oppimine
+FROM Eksam 
+WHERE tulemus IN (4,5)
+AND Eksam.oppimine=Oppimine.oppimine)
+ORDER BY Tudeng.perenimi, Aine.nimetus;
+
+SELECT DISTINCT K.külalise_nr, K.eesnimi, K.perenimi
+FROM Külaline AS K, Reserveerimine AS R
+WHERE K.külalise_nr=R.külalise_nr
+AND R.hotelli_nr IN (SELECT hotelli_nr
+FROM Hotell AS H 
+WHERE nimi = 'Viru')
+ORDER BY K.perenimi, K.eesnimi;
+
+SELECT ROUND(AVG(R.lopu_aeg - R.alguse_aeg),2) AS keskmine
+FROM Hotell AS H, Reserveerimine AS R
+WHERE Year(R.alguse_aeg) IN (2011,2015)
+AND R.hotelli_nr = H.hotelli_nr
+AND H.linn = 'Tallinn'
+
+-- 17.03 praks
+SELECT Tudeng.tudkood, Ucase(Tudeng.perenimi) AS perenimi, Count(*) AS ridade_arv
+FROM (Eksam INNER JOIN Oppimine ON Eksam.oppimine=Oppimine.oppimine)
+INNER JOIN Tudeng ON Tudeng.tudkood=Oppimine.tudeng
+WHERE Eksam.tulemus BETWEEN 1 AND 5
+GROUP BY Tudeng.tudkood, Ucase(Tudeng.perenimi);
+
 
 SELECT * 
-FROM Oppimine
-WHERE Year(oppimis_algus) = 1998;
+FROM (SELECT Tudeng.tudkood, Ucase(Tudeng.perenimi) AS perenimi, Count(*) AS ridade_arv
+FROM (Eksam INNER JOIN Oppimine ON Eksam.oppimine=Oppimine.oppimine)
+INNER JOIN Tudeng ON Tudeng.tudkood=Oppimine.tudeng
+WHERE Eksam.tulemus BETWEEN 1 AND 5
+GROUP BY Tudeng.tudkood, Ucase(Tudeng.perenimi)
+) AS 1703praksül1
+WHERE ridade_arv=(SELECT Max(ridade_arv) as m FROM (SELECT Tudeng.tudkood, Ucase(Tudeng.perenimi) AS perenimi, Count(*) AS ridade_arv
+FROM (Eksam INNER JOIN Oppimine ON Eksam.oppimine=Oppimine.oppimine)
+INNER JOIN Tudeng ON Tudeng.tudkood=Oppimine.tudeng
+WHERE Eksam.tulemus BETWEEN 1 AND 5
+GROUP BY Tudeng.tudkood, Ucase(Tudeng.perenimi)
+) AS 1703praksül1);
+
+SELECT K.külalise_nr, Ucase(K.perenimi) AS perenimi, Count(*) AS res_arv, ROUND(AVG(R.lopu_aeg - R.alguse_aeg),1) AS keskm_res_pikkus
+FROM Külaline AS K, Reserveerimine AS R
+WHERE R.külalise_nr=K.külalise_nr
+GROUP BY K.külalise_nr, Ucase(K.perenimi)
+HAVING Count(*) >= 3
+ORDER BY Count(*) DESC, Ucase(K.perenimi);
+
+SELECT Count(*) AS arv
+FROM (Ruum AS R INNER JOIN Hotell AS H ON R.hotelli_nr=H.hotelli_nr)
+WHERE ((R.hind BETWEEN 100 AND 200) OR R.hind > 300);
+
+
