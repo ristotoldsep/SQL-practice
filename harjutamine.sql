78ec618e1c55f90373d76330b3f96d96b2a788fa
Risto Tõldsep <r.toldsep@gmail.com>
2021-04-07 17:52:21 +0300

    07.04 praktikumi ülesanded

--- harjutamine.sql
+++ harjutamine.sql
@@ -217,3 +217,169 @@ WHERE tulemus=0
 AND E.oppimine=O.oppimine
 GROUP BY tudeng
 HAVING Count(*)>2;
+
+-- kellel pole eksamit tehtud
+SELECT tudeng
+FROM Oppimine LEFT JOIN Eksam ON Oppimine.oppimine=Eksam.oppimine
+WHERE Eksam.oppimine IS NULL;
+
+SELECT Round(toad.arv_tubadega*100/iif(koik.arv_kokku=0, NULL, koik.arv_kokku),1) AS protsent
+FROM (SELECT Count(*) AS arv_tubadega
+FROM Hotell
+WHERE linn='Tallinn'
+AND hotelli_nr IN (SELECT hotelli_nr
+FROM Ruum
+WHERE ruumi_tüüp='Äriklassi tuba')) AS toad,
+(SELECT Count(*) AS arv_kokku
+FROM Hotell
+WHERE linn='Tallinn') AS koik;
+
+SELECT Round(reserveerimised.res_arv*100/iif(koik.arv_kokku=0, NULL, koik.arv_kokku),1) AS protsent
+FROM (SELECT Count(*) AS res_arv
+FROM Reserveerimine AS Re
+WHERE Year(alguse_aeg)=2004
+AND hotelli_nr IN (SELECT hotelli_nr
+FROM Hotell AS H
+WHERE H.linn = 'Tallinn')) AS reserveerimised,
+(SELECT Count(*) AS arv_kokku
+FROM Hotell AS H
+WHERE H.linn='Tallinn') AS koik;
+
+SELECT Round(toad.arv_tubadega*100/iif(koik.arv_kokku=0, NULL, koik.arv_kokku),1) AS protsent
+FROM (SELECT Count(*) AS arv_tubadega
+FROM Hotell
+WHERE linn='Tallinn'
+AND hotelli_nr IN (SELECT hotelli_nr
+FROM Ruum
+WHERE ruumi_tüüp='Äriklassi tuba')) AS toad,
+(SELECT Count(*) AS arv_kokku
+FROM Hotell
+WHERE linn='Tallinn') AS koik;
+
+SELECT Count(*) AS res_arv
+FROM Reserveerimine AS Re, Hotell AS H
+WHERE Re.hotelli_nr = H.hotelli_nr
+AND H.linn = 'Tallinn'
+AND Year(alguse_aeg)=2004
+
+INSERT INTO Ruum_koopia ( ruumi_nr, hotelli_nr, ruumi_tüüp, hind )
+SELECT Ruum.ruumi_nr, Ruum.hotelli_nr, Ruum.ruumi_tüüp, Ruum.hind
+FROM Ruum
+WHERE (((Ruum.[ruumi_nr]) In (SELECT ruumi_nr
+FROM Ruum
+WHERE (Ruum.hind BETWEEN 100 AND 200)
+OR (Ruum.hind>400)))
+AND Ruum.ruumi_tüüp='Äriklassi tuba');
+
+-- ÕIGE ül 3
+SELECT Round(Hotellide_arv.Hot_arv*100/iif(koik.arv_kokku=0, NULL, koik.arv_kokku),1) AS protsent
+FROM (SELECT Count(*) AS Hot_arv
+FROM (SELECT H.hotelli_nr
+FROM Reserveerimine AS Re, Hotell AS H
+WHERE Re.hotelli_nr=H.hotelli_nr
+AND Year(alguse_aeg)=2004
+AND H.linn='Tallinn'
+GROUP BY H.hotelli_nr
+HAVING Count(*)>=1)) AS Hotellide_arv,
+(SELECT Count(*) AS arv_kokku
+FROM Hotell
+WHERE linn='Tallinn') AS koik;
+
+-- 07.04 praktikum
+/* 1. Lisada tabelisse "Tudeng" veerg nimega "kommentaar" (tekstiväli, võimaldab sisestada 
+kuni 150 tähemärki), mittekohustuslik veerg. Lisaks kirjutage lause, mis kustutab veeru "kommentaar"
+tabelist "Tudeng". (15) */
+ALTER TABLE Tudeng
+ADD COLUMN kommentaar VARCHAR(150);
+
+-- Kustuta veerg tabelist
+ALTER TABLE Tudeng
+DROP COLUMN kommentaar;
+
+/* 2. Lisada tudengitele, kelle vastuvõtu aeg on enne  1995. aastat ja kelle õppesuuna tähise 
+kaks esimest tähte on "LZ", kommentaar "Peab varsti lõpetama!". Koostage lause nii, et kui veerus 
+"kommentaar" on juba väärtus olemas, siis lisatakse see tekst olemasoleva teksti algusesse. (15) */ /* Trim eemaldab tühikud, & concatenateb stringid */
+UPDATE Tudeng SET kommentaar=Trim('Peab varsti lõpetama!' & kommentaar)
+WHERE Year(vastuvotu_aeg) < 1995
+AND oppesuund LIKE 'LZ%';
+
+/* 4) Kirjutage lause, millega kustutate kõik read tabelist Külaline_koopia.
+Kirjutage lause, millega deklareerite tabelis Külaline_koopia primaarvõtme (külalise_nr). */
+-- TRUNCATE TABLE Külaline_koopia; EI SOBI accessis
+DELETE FROM Külaline_koopia;
+
+ALTER TABLE Külaline_koopia 
+ADD CONSTRAINT pk_kylaline PRIMARY KEY (külalise_nr);
+
+/* 4) Lisage tabelisse Külaline_koopia andmed kõikide nende külaliste kohta, kes pole teinud mitte
+ühtegi reserveerimist Tartu hotellides. (1) */
+INSERT INTO Külaline_koopia (külalise_nr, eesnimi, perenimi, aadress)
+SELECT külalise_nr, eesnimi, perenimi, aadress
+FROM Külaline
+WHERE külalise_nr NOT IN (
+SELECT külalise_nr
+FROM Reserveerimine
+WHERE hotelli_nr IN (SELECT hotelli_nr
+FROM Hotell
+WHERE linn='Tartu'));
+
+/* 1.
+Looge SELECT ... INTO lause abil koopia tabelist Külaline nime all Külaline_koopia.
+Kandke selle lausega koopiasse üle vaid need külalised, 
+kellel ei ole ühtegi seotud käesoleval aastal alanud (tuleb leida dünaamiliselt) reserveerimist.
+Vihje: kasutage funktsioone Year ja Date.
+
+Deklareerige tabelis Külaline_koopia primaarvõti (külalise_nr) 
+Primaarvõtme kitsenduse nimi: pk_kylaline_koopia.
+
+Kustutage tabelist Külaline_koopia veerg perenimi (muutke tabeli struktuuri).
+
+Kustutage tabel Külaline_koopia. (1) */
+SELECT * INTO Külaline_koopia
+FROM Külaline AS K
+WHERE K.külalise_nr NOT IN (SELECT DISTINCT külalise_nr
+FROM Reserveerimine as R
+WHERE Year(R.alguse_aeg) = Year(Date()));
+
+ALTER TABLE Külaline_koopia
+ADD CONSTRAINT pk_kylaline_koopia PRIMARY KEY (külalise_nr);
+
+ALTER TABLE Külaline_koopia 
+DROP COLUMN perenimi;
+
+DROP TABLE Külaline_koopia;
+
+/* 3.
+Leidke külalised, kes on teinud kõige pikema ja kõige lühema reserveerimise. 
+Näidake külalise numbrit, perenime, reserveerimise pikkust 
+ning neljandas veerus (nimega liik) vastavalt kommentaari "kõige lühem reserveerimine" ja 
+"kõige pikem reserveerimine". 
+Reserveerimise pikkus on selle alguse ja lõpu aja vaheline intervall. (1) */
+SELECT K.külalise_nr, perenimi, (R.lopu_aeg-R.alguse_aeg) AS res_pikkus, 'Kõige lühem reserveerimine'  AS liik
+FROM Külaline AS K, Reserveerimine AS R
+WHERE K.külalise_nr = R.külalise_nr
+AND R.lopu_aeg-R.alguse_aeg = (
+SELECT MIN(R.lopu_aeg-R.alguse_aeg)
+FROM Reserveerimine R)
+UNION
+SELECT K.külalise_nr, perenimi, (R.lopu_aeg-R.alguse_aeg) AS res_pikkus, 'Kõige pikem reserveerimine'  AS liik
+FROM Külaline AS K, Reserveerimine AS R
+WHERE K.külalise_nr = R.külalise_nr
+AND R.lopu_aeg-R.alguse_aeg = (
+SELECT MAX(R.lopu_aeg-R.alguse_aeg)
+FROM Reserveerimine R);
+
+/* 2.
+Looge SELECT ... INTO lause abil koopia tabelist Külaline nime all Külaline_koopia.
+
+Kustutage tabelis Külaline_koopia selliste külaliste aadressid, 
+kellel on kokku rohkem kui viis reserveerimist, aga kes pole viimase 1000 päeva 
+jooksul kordagi alustanud reserveerimist hotellis nimega "Viru". (1) */
+ SELECT *
+FROM Külaline
+WHERE Külaline.külalise_nr IN 
+(SELECT Külalise_nr
+FROM Reserveerimine INNER JOIN Hotell ON
+Reserveerimine.hotelli_nr = Hotell.Hotelli_nr
+WHERE Reserveerimine.alguse_aeg > DATEADD("D", -1000, Date())
+AND Hotell.nimi <> 'Viru')
